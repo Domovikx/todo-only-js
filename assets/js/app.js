c3ce4dbc94bde92cca3bca7050cf340da3b061b5
domovikx <domovikx@gmail.com>
2019-11-01 11:28:19 +0300

    feat: add html structure

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,37 +1,21 @@
 (function app() {
 
-  const rootDiv = document.getElementById('root');
-  rootDiv.innerHTML = 'тест1'
+  // const body = document.body;
+  // body.innerHTML += 'тест1';
 
-  /*
-    header
-      title - ToDo list (only js)
-      link - vk - https://vk.com/domovikx
-      link - gh - https://github.com/Domovikx
-    main
-      aside
-        counts:           
-          - important
-          - done
-          - all
-      search_box
-        search_field
-      add_box
-        form
-          - input-text
-          - input-submit
-      todo_list_box
-        ul
-          li  
-            - text
-            - btn - important
-            - btn - done
-            - btn - del
-    footer
-      svg - icon
-      @DomovikX
-  */
 
+  // function render(div = body) {
+
+  //   div.append(createElementHeader());
+
+  //   function createElementHeader() {
+  //     const div = document.createElement('header');
+  //     div.innerHTML = 'header';
+  //     return div;
+  //   }
+
+  // }
+  // render();
 
 
 
