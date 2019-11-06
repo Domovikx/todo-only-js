71247c75194d20036e9b260985917bc387ecafe8
domovikx <domovikx@gmail.com>
2019-11-06 10:39:55 +0300

    feat: refactoring

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,167 +1,127 @@
 (function app() {
 
-  processing()
+  let state = {
+    countsValue: {
+      countItems: 9,
+      countImportant: 6,
+      countDone: 3,
+    },
+    searchText: '',
+    additionText: '',
+    todoDate: [
+      createNewTodo('123'),
+      createNewTodo('456'),
+      createNewTodo('7 8 9'),
+    ],
+  }
 
-  function processing() {
 
-    let state = {
-      countsValue: {
-        countAll: 9,
-        countImportant: 6,
-        countDone: 3,
-      },
-      searchText: '',
-      additionText: '',
-      todoDate: [
-        createNewTodo('123'),
-        createNewTodo('456'),
-        createNewTodo('7 8 9'),
-      ],
+  window.onload = () => {
+    if (localStorage.getItem('state')) {
+      state = JSON.parse(localStorage.getItem('state'));
+      addition();
+      counts();
+      render(state);
     }
+  }
+
+  window.onunload = () => {
+    localStorage.setItem('state', JSON.stringify(state));
+  };
 
 
-    window.onload = () => {
-      if (localStorage.getItem('state')) {
-        state = JSON.parse(localStorage.getItem('state'));
-        addition();
-        counts();
-        search();
-        render(state);
-      }
+  function addition() {
+    const element = document.getElementById('addition__form');
+    element.addEventListener('submit', eventSubmit);
+
+    function eventSubmit(event) {
+      event.preventDefault();
+      const text = document.getElementById('add');
+      state.todoDate.unshift(createNewTodo(text.value));
+      text.value = '';
+      counts();
+      render(state);
     }
-
-    window.onunload = () => {
-      localStorage.setItem('state', JSON.stringify(state));
-    };
+  }
 
 
-    function addition() {
-      const element = document.getElementById('addition__form');
-      element.addEventListener('submit', eventSubmit);
+  function search() {
+    const element = document.getElementById('search__form');
+    element.addEventListener('submit', eventSubmit);
 
-      function eventSubmit(event) {
-        event.preventDefault();
-        const text = document.getElementById('add');
-        if (text.value === '') return;
-        state.todoDate.unshift(createNewTodo(text.value));
-        text.value = '';
-        counts();
-        render(state);
-      }
+    function eventSubmit(event) {
+      event.preventDefault();
+      const search = document.getElementById('search');
+      if (search.value === '') return render(state);
+      const searchText = search.value.toLowerCase();
+      const filterTodoDate = state.todoDate.filter((obj) => (
+        obj.text
+        .toLowerCase()
+        .indexOf(searchText) !== -1
+      ))
+
+      const filterState = {
+        ...state
+      };
+      filterState.todoDate = filterTodoDate;
+
+      render(filterState);
     }
+  }
+  search();
 
 
-    function search() {
-      const element = document.getElementById('search__form');
-      element.addEventListener('submit', eventSubmit);
+  function counts() {
+    const arr = state.todoDate;
+    let countItems = 0;
+    let countImportant = 0;
+    let countDone = 0;
 
-      function eventSubmit(event) {
-        event.preventDefault();
-        const search = document.getElementById('search');
-        if (search.value === '') return render(state);
-        const searchText = search.value.toLowerCase();
-        const filterTodoDate = state.todoDate.filter((obj) => (
-          obj.text
-          .toLowerCase()
-          .indexOf(searchText) !== -1
-        ))
+    arr.forEach(({
+      important,
+      done
+    }) => {
+      countItems++;
+      if (important) countImportant++;
+      if (done) countDone++;
+    });
 
-        const filterState = {
-          ...state
-        };
-        filterState.todoDate = filterTodoDate;
-
-        render(filterState);
-      }
-    }
+    state.countsValue.countItems = countItems;
+    state.countsValue.countImportant = countImportant;
+    state.countsValue.countDone = countDone;
+  }
 
 
-    function counts() {
+  function listEvents() {
+    const element = document.getElementById('todo__list');
+    element.addEventListener('click', (event) => {
+      const eventValue = event.target.value;
+      const id = event.target.parentNode.id;
       const arr = state.todoDate;
-      const countAll = arr.length;
-      let countImportant = 0;
-      let countDone = 0;
-
-      state.countsValue.countAll = countAll;
 
       for (let i = 0; i < arr.length; i++) {
-        countImportant += state.todoDate[i].important + 0;
+        if (id === arr[i].id) {
+          if (eventValue === 'del') arr.splice(i, 1);listEvents
+          if (eventValue === 'important') arr[i].important = !arr[i].important;
+          if (eventValue === 'done') arr[i].done = !arr[i].done;
+          counts();
+          render(state);
+          return;
+        }
       }
-      state.countsValue.countImportant = countImportant;
+    });
+  }
+  listEvents();
 
-      for (let i = 0; i < arr.length; i++) {
-        countDone += state.todoDate[i].done + 0;
-      }
-      state.countsValue.countDone = countDone;
+
+  function createNewTodo(text) {
+    const doId = () => Math.random().toString(32).substr(2, 16);
+    return {
+      id: doId(),
+      text,
+      important: false,
+      done: false,
     }
-
-
-    (function listEvents() {
-      const element = document.getElementById('todo__list');
-      element.addEventListener('click', (event) => {
-        const value = event.target.value;
-        const id = event.target.parentNode.id;
-        const arr = state.todoDate;
-
-        switch (value) {
-          case 'del':
-            del(id, arr);
-            break;
-          case 'important':
-            important(id, arr);
-            break;
-          case 'done':
-            done(id, arr);
-            break;
-
-          default:
-            break;
-        }
-      });
-
-      function del(id, arr) {
-        for (let i = 0; i < arr.length; i++) {
-          if (id === arr[i].id) {
-            arr.splice(i, 1);
-            counts();
-            render(state);
-          }
-        }
-      }
-
-      function important(id, arr) {
-        for (let i = 0; i < arr.length; i++) {
-          if (id === arr[i].id) {
-            arr[i].important = !arr[i].important;
-            counts();
-            render(state);
-          }
-        }
-      }
-
-      function done(id, arr) {
-        for (let i = 0; i < arr.length; i++) {
-          if (id === arr[i].id) {
-            arr[i].done = !arr[i].done;
-            counts();
-            render(state);
-          }
-        }
-      }
-    })();
-
-
-    function createNewTodo(text) {
-      const doId = () => Math.random().toString(32).substr(2, 16);
-      return {
-        id: doId(),
-        text,
-        important: false,
-        done: false,
-      }
-    }
-
-    render(state);
   }
 
 
@@ -170,7 +130,7 @@
     (function counts() {
       const element = document.getElementById('counts');
       element.innerHTML = `
-      ${state.countsValue.countAll} | 
+      ${state.countsValue.countItems} | 
       ${state.countsValue.countImportant} | 
       ${state.countsValue.countDone}`;
     })();
@@ -214,7 +174,7 @@
          `;
       }
     })();
-
   }
+  render(state);
 
 })()
